aabd286b1d0388eebc06aa5394a880e24ab2ab13
Sandra1887 <93sanjep@molndal.gafe.se>
2023-06-23 12:23:16 +0200

    DbHandler create(), read(), update() tests working

--- src/main/java/org/example/DbHandler.java
+++ src/main/java/org/example/DbHandler.java
@@ -76,35 +76,58 @@ public class DbHandler implements iCrud {
     @Override
     public boolean update() {
         String tableName = helper.askForTableName();
-        String sql = "UPDATE " + tableName + " SET todo = ?, done = ?, assignedTo = ? WHERE todo_id = ?";
         int todoId = helper.askForId();
-        String assignment = helper.askForOnlyAssignment();
-        String assignee = helper.askForAssignee();
-        String done = helper.askForDone();
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
-            pstmt.setString(1, assignment);
-            pstmt.setString(3, assignee);
-            pstmt.setString(2, done);
-            pstmt.setInt(4, todoId);
-            pstmt.executeUpdate();
-            return false;
-        } catch (SQLException e) {
-            System.out.println("Error updating table: " + e.getMessage());
-            return true;
+        String asOrDo = helper.askForUpdate();
+
+        if (asOrDo.equalsIgnoreCase("assignment")) {
+            String sql = "UPDATE " + tableName + " SET assignment = ? WHERE todo_id = ?";
+            String assignment = helper.askForOnlyAssignment();
+            try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setString(1, assignment);
+                pstmt.setInt(2, todoId);
+                pstmt.executeUpdate();
+                return false;
+            } catch (SQLException e) {
+                System.out.println("Error updating table: " + e.getMessage());
+                return true;
+            }
         }
+        if(asOrDo.equalsIgnoreCase("done")) {
+            String sql = "UPDATE " + tableName + " SET done = ? WHERE todo_id = ?";
+            String done = helper.askForDone();
+            try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+                pstmt.setString(1, done);
+                pstmt.setInt(2, todoId);
+                pstmt.executeUpdate();
+                return false;
+            } catch (SQLException e) {
+                System.out.println("Error updating table: " + e.getMessage());
+                return true;
+            }
+        } else System.out.println("Wrong input!");
+        return false;
     }
 
     @Override
     public boolean delete() {
-        return false;
+        String tableName = helper.askForTableName();
+        String sql = "DROP TABLE " + tableName;
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Error dropping table: " + e.getMessage());
+            return false;
+        }
     }
 
     public void createTable(String tableName) {
         String sql = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
                 "todo_id INTEGER PRIMARY KEY AUTOINCREMENT," +
-                "todo VARCHAR(50)," +
-                "done VARCHAR(10)," +
-                "assignedTo VARCHAR(50)" +
+                "assignment VARCHAR(50)," +
+                "assignee VARCHAR(50)," +
+                "done VARCHAR(10)" +
                 ")";
         try (Statement stmt = connection.createStatement()) {
             stmt.execute(sql);
